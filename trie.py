3ccbff409f7a01c36d84e6ee09fd845b3ec8a07b
dokato <raymon92@gmail.com>
2015-05-07 22:54:50 +0200

    new features added: - removing keys - finding in order

--- trie.py
+++ trie.py
@@ -11,12 +11,25 @@ class Trie(object):
             word = word[1:]
             for c in self.root.children:
                 if c.has_value(s):
-                    c.add()
+                    c._add()
                     c.add_word(word)
                     break
             else:
-                self.root.add_child(s)
-                self.root.children[-1].add_word(word)
+                self.root.add_child(s,True)
+                self.root.add_word(s+word)
+    
+    def remove(self,word):
+        self.root.remove(word)
+        
+    def find_by_word(self,word):
+        return self.root.find_by_word(word)
+
+    @property
+    def children(self):
+        return self.root.children
+    
+    def __repr__(self):
+        return 'Root<%s>'%(self.root.children)
 
 class TrieNode(object):
     def __init__(self, val):
@@ -36,11 +49,24 @@ class TrieNode(object):
     def children(self):
         return [x for x in self.__children]
     
-    def add(self):
+    def _add(self):
         self.__count += 1
 
-    def add_child(self,value):
-        self.__children.append(TrieNode(value))
+    def _pop(self):
+        self.__count -= 1
+        if self.__count <=0:
+            return False
+        else:
+            return True
+
+    def add_child(self,value,early_child=False):
+        if early_child:
+            tmpt = TrieNode(value)
+            tmpt.__count -= 1
+            self.__children.append(tmpt)
+        else:
+            self.__children.append(TrieNode(value))
+        self.__children.sort()
     
     def add_word(self,word):
         if len(word)>1:
@@ -48,25 +74,78 @@ class TrieNode(object):
             word = word[1:]
             for c in self.children:
                 if c.has_value(s):
-                    c.add()
+                    c._add()
                     c.add_word(word)
                     break
             else:
-                self.add_child(s)
-                self.children[-1].add_word(word)
+                self.add_child(s,True)
+                self.add_word(s+word)
         else:
             s = word
             for c in self.children:
                 if c.has_value(s):
-                    c.add()
+                    c._add()
                     break
             else:
                 self.add_child(s)
-            
 
     def has_value(self,letter):
         return self.__value == letter
+
+    def remove(self,word):
+        if len(word)>1:
+            s = word[0]
+            word = word[1:]
+            for c in self.children:
+                if c.has_value(s):
+                    if c.remove(word):
+                        if not c._pop():
+                            self.__children.remove(c)
+                        return True
+            else:
+                return False
+        else:
+            s = word
+            for c in self.children:
+                if c.has_value(s):
+                    if not c._pop():
+                        self.__children.remove(c)
+                    return True
+            else:
+                return False
+
+    def find_by_word(self,word):
+        nbr = 0
+        node = self
+        while nbr!=None:
+            s = word[0]
+            word = word[1:]
+            print word, nbr
+            print 's', s, 'child ', node.children
+            for c in node.children:
+                nbr+=c.count
+                if c.has_value(s):
+                    nbr-=c.count
+                    node=c
+                    break
+            else:
+                nbr=None
+            if len(node.children)<=0:
+                break
+        return nbr
         
     def __repr__(self):
         return 'Trie<%s (%i)>'%(self.__value,self.__count)
 
+    def __gt__(self, node2):
+        return self.__value > node2.value
+    
+if __name__ == '__main__':
+    t=Trie()
+    t.add_word('money')
+    t.add_word('power')
+    t.add_word('mouse')
+    t.add_word('paris')
+    t.add_word('kawa')
+    t.remove('madrid')
+    print t.find_by_word('power')
